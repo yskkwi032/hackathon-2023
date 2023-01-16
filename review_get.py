db965232813af063f1369ac8ec028585cf0e7648
yskkwi032 <121997208+yskkwi032@users.noreply.github.com>
2023-01-16 14:10:22 +0900

    Add files via upload

--- review_get.py
+++ review_get.py
@@ -7,10 +7,62 @@ import textwrap
 import csv
 import requests
 import re
-import time
+import numpy as np
+import pandas as pd
+import json
+import pprint
+
+REQUEST_URL = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
+APP_ID="1064925935909133632"
+
+serch_keyword = '洗濯機' #検索ワード
+
+stop_word = ["福袋","スタンド","収納","バッテリー","フィルター","置き台","ラック","カバー","部品","ケース","容器","マット"] #除外する商品名（動作不安定・適宜追加）
+
+#sort = "-reviewCount" #レビュー件数順
+sort = "standard" #楽天標準ソート
+
+serch_params = {
+    "format" : "json",
+    "keyword" : serch_keyword,
+    "applicationId" : [APP_ID],
+    "availability" : 0,
+    "hits" : 30, #取ってくる商品の数　最大30
+    "page" : 1,
+    "sort" : sort
+}
+
+response = requests.get(REQUEST_URL, serch_params)
+result = response.json()
+
+print(type(result))
+i_id = result['Items'][0]['Item']['itemCode'] #商品ID
+i_image = result['Items'][0]['Item']['mediumImageUrls'][0] #商品画像
+i_name = result['Items'][0]['Item']['itemName'] #商品名
+i_url = result['Items'][0]['Item']['itemUrl'] #商品ページURL
+count = result['Items'][0]['Item']['reviewCount'] #レビュー数
+
+flag = 0
+for i in range(100):
+    for w in stop_word:
+        if w in i_name or count > 10:
+            i_id = result['Items'][i+1]['Item']['itemCode']
+            i_name = result['Items'][i+1]['Item']['itemName']
+            i_image = result['Items'][i+1]['Item']['mediumImageUrls'][0]
+            i_url = result['Items'][i+1]['Item']['itemUrl']
+            flag = flag + 1
+        else:
+            flag = 0
+
+    if flag == 0:
+        i = 0 #初期化
+        break
+
+print(i_url)
+
 
 #商品ページ　
-product_url = 'https://item.rakuten.co.jp/nakata/3690/'
+product_url = "https://item.rakuten.co.jp/kadenrand/516583/?iasid=07rpp_10095___eb-lcycb3p0-7x-a766b221-11c9-4c35-927b-2db7ec34a673"
 
 #変えなくていい
 review_base = "https://review.rakuten.co.jp/item/"
@@ -45,17 +97,20 @@ def get_all_reviews(url):
         
         for review in reviews:                              #　取得したレビュー数分だけ処理を繰り返す
             review_list.append(review)                      #　レビュー情報をreview_listに格納
+        
+        if i == 4:
+            break
              
 
         #次のページ取得
         browser.get(url)
 
-        elem_tag_name = browser.find_elements(By.TAG_NAME, "a")
+        elem_tag_name = browser.find_elements(By.TAG_NAME, "a") #aタグを全て取得
 
         elem = []
 
         for ii in elem_tag_name:
-            elem.append(ii.get_attribute('href'))
+            elem.append(ii.get_attribute('href')) #URLを全て取得
 
         with open('data.txt', 'w') as f:
             for j in elem:
@@ -66,17 +121,17 @@ def get_all_reviews(url):
             next_url = []
             for line in ff:
                 if review_base in line:
-                    next_url.append(line)
+                    next_url.append(line) #レビューページにつながるURLを探す
         f.close
 
 
         for k in next_url:
-            if str(k).endswith('/'+str(num+1)+'.1/\n'):
+            if str(k).endswith('/'+str(num+1)+'.1/\n'): #次の15件のリンクを探す
                 next_page = k
                 count += 1
         
         if count == 0:
-            next_page = []
+            next_page = [] #次へがなければ終了
         
         
         # 次のページが存在する場合
@@ -126,15 +181,19 @@ if __name__ == '__main__':
  
     #CSVにレビュー情報の書き出し
     #with open('data/sample.csv','w') as f:　#Windowsでエンコードエラーになる場合があるため下の行に変更
-    with open('sample.csv','w',encoding='utf-8') as f:
+    with open('レビューcsv/'+serch_keyword+'.csv','w',encoding='utf-8') as f:
         writer = csv.writer(f, lineterminator='\n')
+        writer.writerow([i_name])
+        writer.writerow([i_url])
+        writer.writerow(["id","review"])
 
         # 全データを表示
         for i in range(len(review_list)):
             csvlist=[]
             review_text = textwrap.fill(review_list[i].text, 500)
             #データ作成
-            csvlist.append('No.{} : '.format(i+1))      #　便宜上「No.XX」の文字列を作成
+            #csvlist.append('No.{} : '.format(i+1))      #　便宜上「No.XX」の文字列を作成
+            csvlist.append(i_id+",")
             csvlist.append(review_text.strip())         #　レビューテキストの先頭・末尾の空白文字を除去
             # 出力    
             writer.writerow(csvlist)                    
